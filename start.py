d7067345551a9b26a57dd2367b2569b69c344c33
sayary-null <shliakhtinkirill@gmail.com>
2022-07-23 17:26:51 +0300

    в скрипт запуска бота добавлена функция загрузки модулей

--- start.py
+++ start.py
@@ -1,8 +1,10 @@
 import json
+import os
 from discord.ext import commands
 import argparse
 
 bot = commands.Bot(command_prefix='>')
+bot.remove_command('help')
 
 
 @bot.command()
@@ -15,6 +17,8 @@ if __name__ == '__main__':
     parser.add_argument('-d', '--debug', action='store_const', const=False)
     arg = parser.parse_args()
 
+    path = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'script')
+
     with open('config.json', 'r') as file:
         config = json.load(file)
 
@@ -22,4 +26,9 @@ if __name__ == '__main__':
     if arg.test:
         token = config['token_test']
 
+    for item in os.listdir(path):
+        if not item.startswith('_'):
+            bot.load_extension(f'script.{item[:-3]}')
+
+    print('run bot')
     bot.run(token)

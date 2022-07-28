0b98b869a7121648258c7840ac965ee8b1a97843
sayary-null <shliakhtinkirill@gmail.com>
2022-07-28 19:41:10 +0300

    Внесены коррективы по работе с каналами.

--- script/AdminCommand.py
+++ script/AdminCommand.py
@@ -1,7 +1,7 @@
 import discord
 from discord.ext import commands
 import utils.global_variables as gv
-from decorators.decor_command import in_channel, is_admin
+from decorators.decor_command import in_channel, is_admin as d_is_admin
 
 
 class AdminCommand(commands.Cog):
@@ -9,8 +9,7 @@ class AdminCommand(commands.Cog):
         self.bot = bot
 
     @commands.command()
-    @is_admin
-    @in_channel(is_base=True, is_command=True, is_admin=True, is_test=True)
+    @d_is_admin
     async def select_channel_permissions(self, ctx: commands.context.Context):
         if (per := gv.DataBaseClass.get_channel_status(ctx.channel.id)) is not None:
             await ctx.send(per)
@@ -18,22 +17,25 @@ class AdminCommand(commands.Cog):
             await ctx.send('Не установлено')
 
     @commands.command()
-    @is_admin
+    @d_is_admin
     @in_channel(is_test=True)
     async def set_permissions_channel(self, ctx: commands.context.Context,
                                       is_base: bool = False,
                                       is_command: bool = False,
                                       is_admin: bool = False,
-                                      is_test: bool = False):
-        if gv.DataBaseClass.get_channel_status(ctx.channel.id) is None:
-            gv.DataBaseClass.set_channel_status(channel_id=ctx.channel.id,
+                                      is_test: bool = False,
+                                      channel_id: int = None):
+        if channel_id is None:
+            channel_id = ctx.channel.id
+        if gv.DataBaseClass.get_channel_status(channel_id) is None:
+            gv.DataBaseClass.set_channel_status(channel_id=channel_id,
                                                 is_base=is_base,
                                                 is_command=is_command,
                                                 is_admin=is_admin,
                                                 is_test=is_test)
             await ctx.send('Установлено')
         else:
-            gv.DataBaseClass.update_channel_status(channel_id=ctx.channel.id,
+            gv.DataBaseClass.update_channel_status(channel_id=channel_id,
                                                    is_base=is_base,
                                                    is_command=is_command,
                                                    is_admin=is_admin,

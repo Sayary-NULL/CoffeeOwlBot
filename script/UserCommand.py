05e8d947fff8d99a4ec979cc639c9ef109692d71
sayary-null <shliakhtinkirill@gmail.com>
2022-07-31 12:08:19 +0300

    Добавлена функция roll

--- script/UserCommand.py
+++ script/UserCommand.py
@@ -1,5 +1,6 @@
 import json
 import discord
+import random
 from script import AdminCommand, OwnerCommand
 from loguru import logger
 from discord.ext import commands
@@ -12,13 +13,39 @@ class UserCommand(commands.Cog):
     def __init__(self, bot: discord.Client):
         self.bot = bot
 
-    @commands.command()
+    @add_description('Привет 0/')
+    @commands.command(aliases=['привет'])
     @logger.catch
     @in_channel(is_base=True)
     async def hello(self, ctx: commands.context.Context):
         await ctx.send(f'{ctx.author.mention}, hello!')
 
-    @commands.command()
+    @add_description('Выбирает из строки случайный текст, разделенный символом ";"')
+    @commands.command(aliases=['случай', 'random'])
+    @logger.catch
+    @in_channel(is_base=True, is_command=True)
+    async def roll(self, ctx: commands.context.Context, roll_text: str):
+        roll_arr = [roll.strip() for roll in roll_text.split(';')]
+
+        embed = discord.Embed(color=UserColor)
+        random_id = random.randint(0, len(roll_arr)-1)
+        result_text = ''
+
+        for i, text in enumerate(roll_arr):
+            if i == random_id:
+                result_text += f'**{i+1}) {text}**\n'
+            else:
+                result_text += f'{i+1}) {text}\n'
+
+        embed.add_field(
+            name='Результаты',
+            value=result_text,
+            inline=False
+        )
+        await ctx.send(embed=embed)
+
+    @add_description('Показывает текущую версию бота')
+    @commands.command(aliases=['версия'])
     @logger.catch
     @in_channel(is_command=True)
     async def ver(self, ctx: commands.context.Context):

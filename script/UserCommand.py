4c880776b41497873e0fd1cc1d6e03ff6b3d8aac
sayary-null <shliakhtinkirill@gmail.com>
2022-07-31 11:25:45 +0300

    Добавлено расширение для получения информации про определенную функцию.

--- script/UserCommand.py
+++ script/UserCommand.py
@@ -25,6 +25,7 @@ class UserCommand(commands.Cog):
             js = json.load(f)
         await ctx.send(f'версия: {js["ver"]}')
 
+    @add_description('`>help` для большей подробности')
     @commands.command()
     @logger.catch
     @in_channel(is_command=True)
@@ -43,7 +44,61 @@ class UserCommand(commands.Cog):
                 logger.debug(f'owner {rez}')
             await ctx.send(embed=embed)
         else:
-            await ctx.send('HEEELP')
+            embed = discord.Embed(description=f'**Справка по команде {func_name}**', color=discord.colour.Colour.blue())
+            class_name = 'User'
+
+            if (func := get_funcs_on_name_or_aliases(func_name, UserCommand)) is None:
+                class_name = 'Admin'
+                if (func := get_funcs_on_name_or_aliases(func_name, AdminCommand.AdminCommand)) is None:
+                    class_name = 'Owner'
+                    func = get_funcs_on_name_or_aliases(func_name, AdminCommand.AdminCommand)
+
+            if func is None:
+                embed.add_field(
+                    name='**Описание**',
+                    value='Команда не найдена',
+                    inline=False
+                )
+                await ctx.send(embed=embed)
+                return
+
+            embed.add_field(
+                name='**Оригинальное имя**',
+                value=func.name,
+                inline=False
+            )
+
+            embed.add_field(
+                name='**Класс доступа**',
+                value=class_name,
+                inline=False
+            )
+            description = None
+            if 'description' in dir(func):
+                if func.description is not None:
+                    if func.description.strip() != '':
+                        description = func.description
+
+            if description is None:
+                description = 'Не установлено'
+
+            embed.add_field(
+                name='**Описание**',
+                value=description,
+                inline=False
+            )
+
+            if len(func.aliases) != 0:
+                aliases = ''
+                for i, alias in enumerate(func.aliases):
+                    aliases += f'{i + 1}) {alias}\n'
+                embed.add_field(
+                    name='**Псевдонимы**',
+                    value=aliases,
+                    inline=False
+                )
+
+            await ctx.send(embed=embed)
 
 
 def setup(bot):

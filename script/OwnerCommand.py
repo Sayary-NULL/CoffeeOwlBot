0d8058b3e1581d64240d3131e0e74b1341926349
sayary-null <shliakhtinkirill@gmail.com>
2022-08-05 22:01:28 +0300

    Добавлена функция для публекации новостей.

--- script/OwnerCommand.py
+++ script/OwnerCommand.py
@@ -8,15 +8,73 @@ from decorators.decor_command import in_channel, is_owner
 class OwnerCommand(commands.Cog):
     def __init__(self, bot: discord.Client):
         self.bot = bot
+        self.url = 'https://media.discordapp.net/attachments/462236317926031370/464447806887690240/news26052017.jpg' \
+                   '?width=1193&height=671 '
+        self.title = None
+        self.desc = None
+        self.channel_id = None
+        self.sign = "Автор: <@329653972728020994>"
 
     @commands.command()
-    @in_channel(is_test=True)
+    @in_channel(is_admin=True)
     @logger.catch
     @is_owner
-    async def test(self, ctx: commands.context.Context, mess: str):
-        message: discord.Message = ctx.message
-        for item in gv.DataBaseClass.get_trigger_form_text(mess.lower()):
-            await ctx.send(item)
+    async def test(self, ctx: commands.context.Context):
+        await ctx.send('Ok')
+
+    @commands.command()
+    @in_channel(is_admin=True)
+    @logger.catch
+    @is_owner
+    async def post_news(self, ctx: commands.context.Context):
+        if self.desc is None:
+            return
+
+        if self.channel_id is not None:
+            channel: discord.TextChannel = ctx.guild.get_channel(self.channel_id)
+        else:
+            channel = ctx.channel
+
+        await channel.send(self.url)
+        if self.title is not None:
+            await channel.send(self.title)
+        await channel.send(self.desc)
+        await channel.send(self.sign)
+
+    @commands.command()
+    @in_channel(is_admin=True)
+    @logger.catch
+    @is_owner
+    async def add_image(self, ctx: commands.context.Context, url: str):
+        self.url = url
+
+    @commands.command()
+    @in_channel(is_admin=True)
+    @logger.catch
+    @is_owner
+    async def add_title(self, ctx: commands.context.Context, title: str):
+        self.title = title
+
+    @commands.command()
+    @in_channel(is_admin=True)
+    @logger.catch
+    @is_owner
+    async def add_desc(self, ctx: commands.context.Context, desc: str):
+        self.desc = desc
+
+    @commands.command()
+    @in_channel(is_admin=True)
+    @logger.catch
+    @is_owner
+    async def add_sign(self, ctx: commands.context.Context, sign: str):
+        self.sign = sign
+
+    @commands.command()
+    @in_channel(is_admin=True)
+    @logger.catch
+    @is_owner
+    async def set_channel(self, ctx: commands.context.Context, channel_id: int):
+        self.channel_id = channel_id
 
 
 def setup(bot):

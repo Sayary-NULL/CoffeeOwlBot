9ef4de7184e2f0c14a426424cac84f3f7c9f2462
sayary-null <shliakhtinkirill@gmail.com>
2022-09-23 19:50:42 +0300

    Изменение логирования

--- start.py
+++ start.py
@@ -31,7 +31,20 @@ async def reload(ctx: commands.context.Context):
     await ctx.send(f'{ctx.author.mention}, перезагрузка модулей завершена')
 
 
-async def main():
+async def start_bot(token: str):
+    async with bot:
+        logger.info('Start load functions')
+        for item in os.listdir('./script'):
+            if item.startswith('_'):
+                continue
+            logger.debug(f'загрузка модуля: {item[:-3]}')
+            await bot.load_extension(f'script.{item[:-3]}')
+        logger.info('End load functions')
+
+        await bot.start(token)
+
+
+def main():
     parser = argparse.ArgumentParser()
     parser.add_argument('-t', '--test', action='store_const', const=True)
     parser.add_argument('-d', '--debug', action='store_const', const=True)
@@ -45,10 +58,8 @@ async def main():
     gv.DataBaseClass = DB(config['file_db'])
     gv.EnergyVariablesClass = EnergyVariables('variables.data')
     token = config['token_owl']
-    log_dir = 'logs'
 
     if arg.debug or arg.test:
-        logger.debug('Mode BEBUG on')
         logger_level = 'DEBUG'
         gv.ISDebug = True
 
@@ -56,31 +67,18 @@ async def main():
         token = config['token_test']
         logger_level = 'DEBUG'
         gv.TestBot = True
-        log_dir = 'test_logs'
 
-    logger.remove()
-    logger.add(log_dir + '/logs_{time:YY_M_D}.log', format=gv.FormatLog, level=logger_level)
-
-    logger.add(sys.stdout, format=gv.FormatLog, level=logger_level)
+    logger.add('logs/logs_{time:YY_MM_DD}.log', level=logger_level, rotation='1 MB', compression='zip')
 
     with open('version.json', 'r') as f:
         ver = json.load(f)['ver']
 
     logger.info(f'Запуск бота версии: {ver}')
 
-    async with bot:
-        logger.info('Start load functions')
-        for item in os.listdir('./script'):
-            if item.startswith('_'):
-                continue
-            logger.debug(f'загрузка модуля: {item[:-3]}')
-            await bot.load_extension(f'script.{item[:-3]}')
-        logger.info('End load functions')
-
-        await bot.start(token)
+    asyncio.run(start_bot(token))
 
     logger.info('Бот выключен')
 
 
 if __name__ == '__main__':
-    asyncio.run(main())
+    main()

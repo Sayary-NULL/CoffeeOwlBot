f6c388ae24e34bfde3a29ac7e0d0fd1eb9bd3a1c
Sayary <33820978+Sayary-NULL@users.noreply.github.com>
2024-10-18 13:23:26 +0300

    remove limit from text echo OwnerCommand.py

--- script/OwnerCommand.py
+++ script/OwnerCommand.py
@@ -46,7 +46,7 @@ class OwnerCommand(commands.Cog):
     @write_log('ehco')
     @logger.catch
     @checks(is_owner, in_channel(is_base=True))
-    async def echo(self, ctx: commands.context.Context, channel: discord.TextChannel, text):
+    async def echo(self, ctx: commands.context.Context, channel: discord.TextChannel,  *, text: str):
         await channel.send(text)
 
     @commands.command()
